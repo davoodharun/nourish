a0301c377410bba331992c57793d268465ad6234
Harun Davood <davoodharun@gmail.com>
2016-03-16 19:01:15 -0400

    refactor to generic class based views

---
+++ stores/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from stores import views
+
+urlpatterns = [
+    url(r'^stores/$', views.StoreList.as_view()),
+    url(r'^stores/(?P<pk>[0-9]+)/$', views.StoreDetail.as_view()),
+    url(r'^items/$', views.ItemList.as_view()),
+    url(r'^items/(?P<pk>[0-9]+)/$', views.ItemDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- stores/views.py
+++ stores/views.py
@@ -1,97 +1,20 @@
-from rest_framework import status
-from rest_framework.response import Response
-from stores.models import Store, Item
-from stores.serializers import StoreSerializer, ItemSerializer
-from django.http import Http404
-from rest_framework.views import APIView
+from models import Store, Item
+from serializers import StoreSerializer, ItemSerializer
+from rest_framework import generics
 
 
-class StoreList(APIView):
-    """
-    List all stores, or create a new store.
-    """
-    def get(self, request, format=None):
-        stores = Store.objects.all()
-        serializer = StoreSerializer(stores, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = StoreSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class StoreDetail(APIView):
-    """
-    Retrieve, update or delete a store instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Store.objects.get(pk=pk)
-        except Store.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        store = self.get_object(pk)
-        serializer = StoreSerializer(store)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        store = self.get_object(pk)
-        serializer = StoreSerializer(store, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        store = self.get_object(pk)
-        store.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class StoreList(generics.ListCreateAPIView):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
 
 
-class ItemList(APIView):
-    """
-    List all stores, or create a new item.
-    """
-    def get(self, request, format=None):
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data)
+class StoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Store.objects.all()
 
-    def post(self, request, format=None):
-        serializer = ItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ItemList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
 
-class ItemDetail(APIView):
-    """
-    Retrieve, update or delete a item instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Item.objects.get(pk=pk)
-        except Item.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk, format=None):
-        item = self.get_object(pk)
-        serializer = ItemSerializer(item)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        item = self.get_object(pk)
-        serializer = ItemSerializer(item, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        item = self.get_object(pk)
-        item.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
