52b853865d7185ed3951d5eda2b545d45f7d4e4a
Harun Davood <davoodharun@gmail.com>
2016-03-21 06:44:21 -0400

    heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',               
-        'PORT': '',       
+        'PORT': '5000',       
     }
 }
 
